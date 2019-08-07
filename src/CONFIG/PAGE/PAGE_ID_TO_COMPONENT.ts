9e3a85b67eb01cac131e661baa9ccebaf88ccc38
Soulike <316413310@qq.com>
2019-08-07 21:38:19 +0800

    完成登录页面
    - 完成登录页面布局与逻辑
    - 完成 Account Api

--- src/CONFIG/PAGE/PAGE_ID_TO_COMPONENT.ts
+++ src/CONFIG/PAGE/PAGE_ID_TO_COMPONENT.ts
@@ -1,7 +1,10 @@
 import {PAGE_ID} from './PAGE_ID';
+import React from 'react';
+
+const Login = React.lazy(() => import('../../Page/Login'));
 
 export const PAGE_ID_TO_COMPONENT = {
-    [PAGE_ID.LOGIN]: undefined,
+    [PAGE_ID.LOGIN]: Login,
 
     [PAGE_ID.MANAGE.INDEX]: undefined,
 
---
+++ src/Page/Login/Container.tsx
@@ -0,0 +1,66 @@
+import React, {ChangeEvent, FormEvent, PureComponent} from 'react';
+import LoginView from './View';
+import {PAGE_ID, PAGE_ID_TO_NAME, PAGE_ID_TO_ROUTE, ROUTE_TO_PAGE_ID} from '../../CONFIG/PAGE';
+import {RouteComponentProps, withRouter} from 'react-router-dom';
+import {login} from '../../Api';
+import {message} from 'antd';
+
+interface Props extends RouteComponentProps {}
+
+interface State
+{
+    username: string,
+    password: string,
+}
+
+class Login extends PureComponent<Props, State>
+{
+    constructor(props: Props)
+    {
+        super(props);
+        this.state = {
+            username: '',
+            password: '',
+        };
+    }
+
+    componentDidMount()
+    {
+        document.title = PAGE_ID_TO_NAME[ROUTE_TO_PAGE_ID[this.props.location.pathname]];
+    }
+
+    onUsernameInputChange = (e: ChangeEvent<HTMLInputElement>) =>
+    {
+        this.setState({username: e.target.value});
+    };
+
+    onPasswordInputChange = (e: ChangeEvent<HTMLInputElement>) =>
+    {
+        this.setState({password: e.target.value});
+    };
+
+    onLoginFormSubmit = async (e: FormEvent<HTMLFormElement>) =>
+    {
+        e.preventDefault();
+        const {username, password} = this.state;
+        const isSuccessful = await login(username, password);
+        if (isSuccessful)
+        {
+            message.success('登录成功');
+            this.props.history.push(PAGE_ID_TO_ROUTE[PAGE_ID.MANAGE.INDEX]);
+        }
+    };
+
+    render()
+    {
+        const {username, password} = this.state;
+        return (
+            <LoginView username={username} password={password}
+                       onLoginFormSubmit={this.onLoginFormSubmit}
+                       onPasswordInputChange={this.onPasswordInputChange}
+                       onUsernameInputChange={this.onUsernameInputChange} />
+        );
+    }
+}
+
+export default withRouter(Login);
