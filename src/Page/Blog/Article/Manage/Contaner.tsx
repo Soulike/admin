3ccc13f60b29da40225609a0459812b551aa5ccd
Soulike <316413310@qq.com>
2019-08-12 23:46:53 +0800

    文章管理页完善
    - 修改文章数组为文章映射
    - 添加标签显示
    - 删除文章按钮完成
    - 文章内容查看完成
    - 列表内容获取完成

--- src/Page/Blog/Article/Manage/Contaner.tsx
+++ src/Page/Blog/Article/Manage/Contaner.tsx
@@ -1,11 +1,25 @@
-import React, {PureComponent} from 'react';
+import React, {DOMAttributes, PureComponent} from 'react';
 import View from './View';
-import {Article} from '../../../../Class';
-import faker from 'faker';
+import {Article, Category} from '../../../../Class';
+import {ModalProps} from 'antd/lib/modal';
+import {markdownConverter} from '../../../../Singleton';
+import {message, notification} from 'antd';
+import {deleteArticleById, getAllArticle} from '../../../../Api/Blog/Article';
+import {getAllCategory} from '../../../../Api/Blog/Category';
+import {PopconfirmProps} from 'antd/lib/popconfirm';
+import {NativeButtonProps} from 'antd/lib/button/button';
 
 interface State
 {
-    articleList: Array<Article>;
+    articleMap: Map<number, Article>;
+    categoryMap: Map<number, Category>,
+    isLoading: boolean,
+
+    articleInModalTitle: string,
+    articleInModalHTMLContent: string,
+    modalIsVisible: boolean,
+
+    idOfArticleToDelete: number,
 }
 
 interface Props
@@ -19,36 +33,129 @@ class Manage extends PureComponent<Props, State>
     {
         super(props);
         this.state = {
-            articleList: [],
+            articleMap: new Map<number, Article>(),
+            categoryMap: new Map<number, Category>(),
+            isLoading: false,
+
+            articleInModalTitle: '',
+            articleInModalHTMLContent: '',
+            modalIsVisible: false,
+
+            idOfArticleToDelete: 0,
         };
+    }
+
+    setStatePromise<K extends keyof State>(state: ((prevState: Readonly<State>, props: Readonly<Props>) => (Pick<State, K> | State | null)) | Pick<State, K> | State | null): Promise<void>
+    {
+        return new Promise<void>(resolve =>
+        {
+            this.setState(state, () =>
+            {
+                resolve();
+            });
+        });
     }
 
     componentDidMount()
     {
-        // 调试代码
-        faker.locale = 'zh_CN';
-        const articleList: Array<Article> = [];
-        for (let i = 1; i < 101; i++)
-        {
-            articleList.push(new Article(
-                i,
-                faker.lorem.sentence(),
-                faker.lorem.paragraphs(),
-                1,
-                faker.date.past().toString(),
-                faker.date.past().toString(),
-                faker.random.number(),
-                faker.random.boolean(),
-            ));
-        }
-        this.setState({articleList});
+        this.setStatePromise({isLoading: true})
+            .then(() =>
+            {
+                return Promise.all([
+                    getAllArticle(),
+                    getAllCategory(),
+                ]);
+            })
+            .then(([articleList, categoryList]) =>
+            {
+                if (articleList !== null && categoryList !== null)
+                {
+                    const articleMap: Map<number, Article> = new Map<number, Article>();
+                    const categoryMap: Map<number, Category> = new Map<number, Category>();
+                    articleList.forEach(article =>
+                    {
+                        articleMap.set(article.id!, article);
+                    });
+                    categoryList.forEach(category =>
+                    {
+                        categoryMap.set(category.id!, category);
+                    });
+
+                    return this.setStatePromise({articleMap, categoryMap});
+                }
+            })
+            .then(() =>
+            {
+                return this.setStatePromise({isLoading: false});
+            });
     }
 
+    modalOnOk: ModalProps['onOk'] = () =>
+    {
+        const {modalIsVisible} = this.state;
+        this.setState({modalIsVisible: !modalIsVisible});
+    };
+
+    modalOnCancel: ModalProps['onCancel'] = this.modalOnOk;
+
+    onArticleTitleClick: (id: number) => DOMAttributes<HTMLSpanElement>['onClick'] = (id: number) =>
+    {
+        return e =>
+        {
+            e.preventDefault();
+            const {articleMap} = this.state;
+            const article = articleMap.get(id);
+            if (typeof article === 'undefined')
+            {
+                message.warning('文章不存在');
+            }
+            else
+            {
+                this.setState({
+                    articleInModalTitle: article.title!,
+                    articleInModalHTMLContent: markdownConverter.makeHtml(article.content!),
+                    modalIsVisible: true,
+                });
+            }
+        };
+    };
+
+    onDeleteArticleButtonClick: (id: number) => NativeButtonProps['onClick'] = (id: number) =>
+    {
+        return () =>
+        {
+            this.setState({idOfArticleToDelete: id});
+        };
+    };
+
+    onDeleteArticleConfirm: PopconfirmProps['onConfirm'] = async () =>
+    {
+        const {idOfArticleToDelete, articleMap} = this.state;
+        const result = await deleteArticleById(idOfArticleToDelete);
+        if (result !== null)
+        {
+            notification.success({
+                message: '文章删除成功',
+            });
+            articleMap.delete(idOfArticleToDelete);
+            this.forceUpdate();
+        }
+    };
 
     render()
     {
-        const {articleList} = this.state;
-        return (<View articleList={articleList} />);
+        const {articleMap, categoryMap, modalIsVisible, articleInModalHTMLContent, articleInModalTitle, isLoading} = this.state;
+        return (<View isLoading={isLoading}
+                      articleMap={articleMap}
+                      categoryMap={categoryMap}
+                      modalIsVisible={modalIsVisible}
+                      articleInModalTitle={articleInModalTitle}
+                      articleInModalHTMLContent={articleInModalHTMLContent}
+                      modalOnOk={this.modalOnOk}
+                      modalOnCancel={this.modalOnCancel}
+                      onArticleTitleClick={this.onArticleTitleClick}
+                      onDeleteArticleButtonClick={this.onDeleteArticleButtonClick}
+                      onDeleteArticleConfirm={this.onDeleteArticleConfirm} />);
     }
 }
 
--- src/Page/Blog/Article/Manage/View.tsx
+++ src/Page/Blog/Article/Manage/View.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, {DOMAttributes} from 'react';
 import Style from './Style.module.scss';
-import {Article} from '../../../../Class';
-import {Button, Icon, List, Switch, Tag, Tooltip} from 'antd';
+import {Article, Category} from '../../../../Class';
+import {Button, Icon, List, Popconfirm, Skeleton, Switch, Tag, Tooltip} from 'antd';
+import ArticlePreviewModal from '../../../../Component/ArticlePreviewModal/View';
+import {ModalProps} from 'antd/lib/modal';
+import {PopconfirmProps} from 'antd/lib/popconfirm';
+import {NativeButtonProps} from 'antd/lib/button/button';
 
 const {Item} = List;
 const {Meta} = Item;
@@ -9,66 +13,85 @@ const {Group} = Button;
 
 interface Props
 {
-    articleList: Array<Article>
+    articleMap: Map<number, Article>,
+    categoryMap: Map<number, Category>,
+    isLoading: boolean,
+
+    onArticleTitleClick: (id: number) => DOMAttributes<HTMLSpanElement>['onClick'],
+    articleInModalTitle: string,
+    articleInModalHTMLContent: string,
+    modalIsVisible: boolean,
+    modalOnOk: ModalProps['onOk'],
+    modalOnCancel: ModalProps['onCancel'],
+
+    onDeleteArticleButtonClick: (id: number) => NativeButtonProps['onClick'],
+    onDeleteArticleConfirm: PopconfirmProps['onConfirm'],
 }
 
 function ManageView(props: Props)
 {
-    const {articleList} = props;
+    const {articleMap, categoryMap, isLoading, articleInModalHTMLContent, articleInModalTitle, modalIsVisible, modalOnCancel, modalOnOk, onArticleTitleClick, onDeleteArticleButtonClick, onDeleteArticleConfirm} = props;
     return (
         <div className={Style.Manage}>
-            <List dataSource={articleList} bordered={true} pagination={
+            <Skeleton loading={isLoading} active={true} paragraph={{rows: 15}}>
+                <List dataSource={Array.from(articleMap.values())} bordered={true} pagination={
+                    {
+                        position: 'bottom',
+                        pageSizeOptions: ['10', '15', '20'],
+                        showSizeChanger: true,
+                    }
+                } renderItem={article =>
                 {
-                    position: 'bottom',
-                    pageSizeOptions: ['10', '15', '20'],
-                    showSizeChanger: true,
-
-                }
-            } renderItem={article =>
-            {
-                const {id, title, isVisible, publicationTime: publicationTimeString, modificationTime: modificationTimeString} = article;
-                const publicationTime = new Date(publicationTimeString!);
-                const modificationTime = new Date(modificationTimeString!);
-                return (
-                    <Item key={id}>
-                        <Meta title={
-                            <span>{title}</span>
-                        } />
-                        <Tag>
-                            发表：
-                            <time>
-                                {`${publicationTime.getFullYear()}-${(publicationTime.getMonth() + 1).toString().padStart(2, '0')}-${(publicationTime.getDate()).toString().padStart(2, '0')}`}
-                            </time>
-                        </Tag>
-                        <Tag>
-                            编辑：
-                            <time>
-                                {`${modificationTime.getFullYear()}-${(modificationTime.getMonth() + 1).toString().padStart(2, '0')}-${(modificationTime.getDate()).toString().padStart(2, '0')}`}
-                            </time>
-                        </Tag>
-                        <Tooltip title={'更改文章可见性'}>
-                            <Switch className={Style.switch}
-                                    checked={isVisible}
-                                    checkedChildren={'可见'}
-                                    unCheckedChildren={'隐藏'} />
-                        </Tooltip>
-                        <Group size={'small'} className={Style.buttonWrapper}>
-                            <Tooltip title={'编辑文章'}>
-                                <Button type={'primary'} ghost={true}>
-                                    <Icon type="edit" />
-                                </Button>
+                    const {id, title, isVisible, publicationTime: publicationTimeString, modificationTime: modificationTimeString} = article;
+                    const publicationTime = new Date(publicationTimeString!);
+                    const modificationTime = new Date(modificationTimeString!);
+                    return (
+                        <Item key={id}>
+                            <Meta title={
+                                <span className={Style.title} onClick={onArticleTitleClick(id!)}>{title}</span>
+                            } />
+                            <Tag color={'blue'}>{categoryMap.get(articleMap.get(id!)!.category!)!.name}</Tag>
+                            <Tag color={'geekblue'}>
+                                发表：
+                                <time>
+                                    {`${publicationTime.getFullYear()}-${(publicationTime.getMonth() + 1).toString().padStart(2, '0')}-${(publicationTime.getDate()).toString().padStart(2, '0')}`}
+                                </time>
+                            </Tag>
+                            <Tag color={'geekblue'}>
+                                编辑：
+                                <time>
+                                    {`${modificationTime.getFullYear()}-${(modificationTime.getMonth() + 1).toString().padStart(2, '0')}-${(modificationTime.getDate()).toString().padStart(2, '0')}`}
+                                </time>
+                            </Tag>
+                            <Tooltip title={'更改文章可见性'}>
+                                <Switch className={Style.switch}
+                                        checked={isVisible}
+                                        checkedChildren={'可见'}
+                                        unCheckedChildren={'隐藏'} />
                             </Tooltip>
-                            <Tooltip title={'删除文章'}>
-                                <Button type={'danger'} ghost={true}>
-                                    <Icon type="delete" />
-                                </Button>
-                            </Tooltip>
-                        </Group>
-                    </Item>);
-            }} />
-        </div>
-    )
-        ;
+                            <Group size={'small'} className={Style.buttonWrapper}>
+                                <Tooltip title={'编辑文章'}>
+                                    <Button type={'primary'} ghost={true}>
+                                        <Icon type="edit" />
+                                    </Button>
+                                </Tooltip>
+                                <Tooltip title={'删除文章'}>
+                                    <Popconfirm title={`确认删除文章《${title}》？`} onConfirm={onDeleteArticleConfirm}>
+                                        <Button type={'danger'} ghost={true} onClick={onDeleteArticleButtonClick(id!)}>
+                                            <Icon type="delete" />
+                                        </Button>
+                                    </Popconfirm>
+                                </Tooltip>
+                            </Group>
+                        </Item>);
+                }} />
+            </Skeleton>
+            <ArticlePreviewModal title={articleInModalTitle}
+                                 HTMLContent={articleInModalHTMLContent}
+                                 visible={modalIsVisible}
+                                 onOk={modalOnOk}
+                                 onCancel={modalOnCancel} />
+        </div>);
 }
 
-export default React.memo(ManageView);
+export default ManageView;
