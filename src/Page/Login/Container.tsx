66aec0e2d996a7eb356ef6c9b5f8ab2d21746b30
Soulike <soulike@qq.com>
2021-02-11 18:24:44 +0800

    使用 Hooks 重构 Login

--- src/Page/Login/Container.tsx
+++ src/Page/Login/Container.tsx
@@ -1,75 +1,61 @@
-import React, {DOMAttributes, PureComponent} from 'react';
-import LoginView from './View';
-import {PAGE_ID, PAGE_ID_TO_ROUTE} from '../../CONFIG/PAGE';
-import {RouteComponentProps, withRouter} from 'react-router-dom';
+import React, {DOMAttributes, useEffect, useState} from 'react';
+import View from './View';
+import {PAGE_ID, PAGE_ID_TO_ROUTE} from '../../CONFIG';
+import {useHistory} from 'react-router-dom';
 import {Account} from '../../Api';
-import {notification} from 'antd';
 import {InputProps} from 'antd/lib/input';
+import {notification} from 'antd';
 
-interface Props extends RouteComponentProps {}
-
-interface State
+function Login()
 {
-    username: string,
-    password: string,
-}
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
-class Login extends PureComponent<Props, State>
-{
-    constructor(props: Props)
-    {
-        super(props);
-        this.state = {
-            username: '',
-            password: '',
-        };
-    }
+    const history = useHistory();
 
-    async componentDidMount()
+    useEffect(() =>
     {
-        const res = await Account.checkSession();
-        if (res !== null)
-        {
-            const {isInSession} = res;
-            if (isInSession)
+        Account.checkSession()
+            .then(res =>
             {
-                this.props.history.replace(PAGE_ID_TO_ROUTE[PAGE_ID.MANAGE.INDEX]);
-            }
-        }
-    }
+                if (res !== null)
+                {
+                    const {isInSession} = res;
+                    if (isInSession)
+                    {
+                        history.replace(PAGE_ID_TO_ROUTE[PAGE_ID.MANAGE.INDEX]);
+                    }
+                }
+            });
+    }, [history]);
 
-    onUsernameInputChange: InputProps['onChange'] = e =>
+    const onUsernameInputChange: InputProps['onChange'] = e =>
     {
-        this.setState({username: e.target.value});
+        setUsername(e.target.value);
     };
 
-    onPasswordInputChange: InputProps['onChange'] = e =>
+    const onPasswordInputChange: InputProps['onChange'] = e =>
     {
-        this.setState({password: e.target.value});
+        setPassword(e.target.value);
     };
 
-    onLoginFormSubmit: DOMAttributes<HTMLFormElement>['onSubmit'] = async e =>
+    const onLoginFormSubmit: DOMAttributes<HTMLFormElement>['onSubmit'] = async e =>
     {
         e.preventDefault();
-        const {username, password} = this.state;
         const isSuccessful = await Account.login(username, password);
         if (isSuccessful)
         {
             notification.success({message: '登录成功'});
-            this.props.history.push(PAGE_ID_TO_ROUTE[PAGE_ID.MANAGE.INDEX]);
+            history.push(PAGE_ID_TO_ROUTE[PAGE_ID.MANAGE.INDEX]);
         }
     };
 
-    render()
-    {
-        const {username, password} = this.state;
-        return (
-            <LoginView username={username} password={password}
-                       onLoginFormSubmit={this.onLoginFormSubmit}
-                       onPasswordInputChange={this.onPasswordInputChange}
-                       onUsernameInputChange={this.onUsernameInputChange} />
-        );
-    }
+    return (
+        <View username={username} password={password}
+              onLoginFormSubmit={onLoginFormSubmit}
+              onPasswordInputChange={onPasswordInputChange}
+              onUsernameInputChange={onUsernameInputChange} />
+    );
 }
 
-export default withRouter(Login);
+export default React.memo(Login);
