7573126911c224b6d75f1cd54c4727fec46df476
Soulike <soulike@qq.com>
2021-02-11 18:16:05 +0800

    使用 Hooks 重构 Frame

--- src/Component/Frame/Container.tsx
+++ src/Component/Frame/Container.tsx
@@ -1,45 +1,41 @@
-import React, {PureComponent} from 'react';
+import React from 'react';
 import View from './View';
 import {NativeButtonProps} from 'antd/lib/button/button';
 import {Modal, notification} from 'antd';
 import {ModalFuncProps} from 'antd/lib/modal';
-import {logout} from '../../Api/Account';
-import {RouteComponentProps, withRouter} from 'react-router-dom';
-import {PAGE_ID, PAGE_ID_TO_ROUTE} from '../../CONFIG/PAGE';
+import {Account as AccountApi} from '../../Api';
+import {useHistory} from 'react-router-dom';
+import {PAGE_ID, PAGE_ID_TO_ROUTE} from '../../CONFIG';
 
-interface Props extends RouteComponentProps
+interface IProps
 {
     children?: React.ReactNode
 }
 
-interface State {}
-
-class Frame extends PureComponent<Props, State>
+function Frame(props: IProps)
 {
-    onExitButtonClick: NativeButtonProps['onClick'] = e =>
+    const history = useHistory();
+
+    const onExitModalOkButtonClick: ModalFuncProps['onOk'] = async () =>
+    {
+        await AccountApi.logout();
+        notification.success({
+            message: '退出成功',
+        });
+        history.replace(PAGE_ID_TO_ROUTE[PAGE_ID.LOGIN]);
+    };
+
+    const onExitButtonClick: NativeButtonProps['onClick'] = e =>
     {
         e.preventDefault();
         Modal.confirm({
             content: '确认退出？',
-            onOk: this.onExitModalOkButtonClick,
+            onOk: onExitModalOkButtonClick,
         });
     };
 
-    onExitModalOkButtonClick: ModalFuncProps['onOk'] = async () =>
-    {
-        await logout();
-        notification.success({
-            message: '退出成功',
-        });
-        const {history} = this.props;
-        history.replace(PAGE_ID_TO_ROUTE[PAGE_ID.LOGIN]);
-    };
-
-    render()
-    {
-        const {children} = this.props;
-        return (<View onExitButtonClick={this.onExitButtonClick}>{children}</View>);
-    }
+    const {children} = props;
+    return (<View onExitButtonClick={onExitButtonClick}>{children}</View>);
 }
 
-export default withRouter(Frame);
+export default React.memo(Frame);
