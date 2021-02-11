6ccff83cf6f422eb7ab4aa65b6d1684d538d52da
Soulike <soulike@qq.com>
2021-02-11 18:09:57 +0800

    Updates

--- static/js/19.30a3558b.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpadmin=this.webpackJsonpadmin||[]).push([[19],{339:function(a,e,c){a.exports={BlogIndex:"Style_BlogIndex__1Qixj",image:"Style_image__2stfc"}},556:function(a,e,c){"use strict";c.r(e);var n=c(0),i=c.n(n),s=c(339),t=c.n(s),m=c.p+"static/media/macbook.52cc9c0c.svg",o=c(18);function d(){return Object(o.jsx)("div",{className:t.a.BlogIndex,children:Object(o.jsx)("img",{className:t.a.image,src:m,alt:"manage_index"})})}var l=i.a.memo(d);e.default=l}}]);
-//# sourceMappingURL=19.30a3558b.chunk.js.map
